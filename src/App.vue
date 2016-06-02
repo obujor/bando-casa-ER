f79639ad59512cb36cbf376d7496ed3b22c79299
Octavian Bujor <ottavio.bo@gmail.com>
2016-06-02 18:57:46 +0200

    Added price and size filters

--- src/App.vue
+++ src/App.vue
@@ -7,23 +7,63 @@
           <a class="ui image" href="/"><img class="logo" src="./assets/logo.png"></a>
         </div>
         <div class="item">
-          <h3>Filtra</h3>
-          <form class="ui form">
-            <div class="field">
-              <label>Provincia</label>
-              <select multiple="multiple" class="ui dropdown" v-model="proviceFilter">
-                <option value="">Tutte</option>
-                <option v-for="province in provinces" value="{{ province }}">{{ province }}</option>
-              </select>
+          <h3>Filtri</h3>
+          <div class="ui styled fluid accordion">
+            <div class="title">
+              <i class="dropdown icon"></i>
+              Ubicazione
             </div>
-            <div class="field">
-              <label>Città</label>
-              <select multiple="multiple" class="ui dropdown" v-model="cityFilter">
-                <option value="">Tutte</option>
-                <option v-for="city in cities" value="{{ city }}">{{ city | capitalizeWords }}</option>
-              </select>
+            <div class="content">
+              <form class="ui form">
+                <div class="field">
+                  <label>Provincia</label>
+                  <select multiple="multiple" class="ui dropdown" v-model="proviceFilter">
+                    <option value="">Tutte</option>
+                    <option v-for="province in provinces" value="{{ province }}">{{ province }}</option>
+                  </select>
+                </div>
+                <div class="field">
+                  <label>Città</label>
+                  <select multiple="multiple" class="ui dropdown" v-model="cityFilter">
+                    <option value="">Tutte</option>
+                    <option v-for="city in cities" value="{{ city }}">{{ city | capitalizeWords }}</option>
+                  </select>
+                </div>
+              </form>
             </div>
-          </form>
+            <div class="title">
+              <i class="dropdown icon"></i>
+              Prezzo
+            </div>
+            <div class="content">
+              <form class="ui form">
+                <div class="field">
+                  <label>Complessivo</label>
+                  <input type="text" id="priceSlider" value="" />
+                </div>
+                <div class="field">
+                  <label>Metro quadrato</label>
+                  <input type="text" id="pricemqSlider" value="" />
+                </div>
+              </form>
+            </div>
+            <div class="title">
+              <i class="dropdown icon"></i>
+              Dimensione
+            </div>
+            <div class="content">
+              <form class="ui form">
+                <div class="field">
+                  <label>Locali</label>
+                  <input type="text" id="locSlider" value="" />
+                </div>
+                <div class="field">
+                  <label>Superficie</label>
+                  <input type="text" id="mqSlider" value="" />
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
         <div class="item">
           <h3>Risultati</h3>
@@ -43,11 +83,9 @@
 <script>
 import HouseList from './components/HouseList'
 import GMapview from './components/GMapview'
-
 import LZString from 'lz-string'
 import $ from 'jquery'
 import _ from 'underscore'
-window.$ = $
 import 'semantic-ui-reset/reset.css'
 import 'semantic-ui-site/site.css'
 import 'semantic-ui-container/container.css'
@@ -66,12 +104,19 @@ import 'semantic-ui-dimmer/dimmer.css'
 import 'semantic-ui-list/list.css'
 import 'semantic-ui-progress/progress.css'
 import 'semantic-ui-button/button.css'
+import 'semantic-ui-accordion/accordion.css'
+import 'ion-rangeslider/css/ion.rangeSlider.css'
+import 'ion-rangeslider/css/ion.rangeSlider.skinFlat.css'
+require('ion-rangeslider')
+
+window.$ = $
 
 $.fn.transition = require('semantic-ui-transition')
 $.fn.dropdown = require('semantic-ui-dropdown')
 $.fn.visibility = require('semantic-ui-visibility')
 $.fn.dimmer = require('semantic-ui-dimmer')
 $.fn.progress = require('semantic-ui-progress')
+$.fn.accordion = require('semantic-ui-accordion')
 
 export default {
   components: {
@@ -82,7 +127,11 @@ export default {
     return {
       allHouses: [],
       proviceFilter: [],
-      cityFilter: []
+      cityFilter: [],
+      priceRange: [],
+      priceMqRange: [],
+      locRange: [],
+      mqRange: []
     }
   },
   computed: {
@@ -90,6 +139,14 @@ export default {
       return _.uniq(this.allHouses.map(function (h) {
         return h.indirizzo[3]
       })).sort()
+    },
+    maxValues: function () {
+      return {
+        pr: _.max(_.pluck(this.allHouses, 'prezzo')),
+        prMq: _.max(_.pluck(this.allHouses, 'prezzoMq')),
+        loc: _.max(_.pluck(this.allHouses, 'locali')),
+        mq: _.max(_.pluck(this.allHouses, 'mq'))
+      }
     },
     cities: function () {
       var provSelected = this.proviceFilter
@@ -104,25 +161,49 @@ export default {
       })).sort()
     },
     housesFiltered: function () {
-      var provSelected = this.proviceFilter
-      var citySelected = this.cityFilter
+      const provSelected = this.proviceFilter
+      const citySelected = this.cityFilter
+      const priceRange = this.priceRange
+      const priceMqRange = this.priceMqRange
+      const locRange = this.locRange
+      const mqRange = this.mqRange
+
+      const rangeFilter = (attr, range, item) => {
+        return item[attr] >= range[0] && item[attr] <= range[1]
+      }
+
       var houses = this.allHouses
-
-      this.$broadcast('filtersChanged')
-
       console.time('filter')
       if (provSelected.length) {
-        houses = houses.filter(function (h) {
+        houses = houses.filter((h) => {
           return provSelected.indexOf(h.indirizzo[3]) !== -1
         })
       }
       if (citySelected.length) {
-        houses = houses.filter(function (h) {
+        houses = houses.filter((h) => {
           return citySelected.indexOf(h.indirizzo[2]) !== -1
         })
       }
+      if (priceRange.length) {
+        houses = houses.filter(rangeFilter.bind(this, 'prezzo', priceRange))
+      }
+      if (priceMqRange.length) {
+        houses = houses.filter(rangeFilter.bind(this, 'prezzoMq', priceMqRange))
+      }
+      if (locRange.length) {
+        houses = houses.filter(rangeFilter.bind(this, 'locali', locRange))
+      }
+      if (mqRange.length) {
+        houses = houses.filter(rangeFilter.bind(this, 'mq', mqRange))
+      }
       console.timeEnd('filter')
+      this.$broadcast('filtersChanged')
       return houses
+    }
+  },
+  watch: {
+    maxValues: function () {
+      this.setupSliders()
     }
   },
   ready: function () {
@@ -139,53 +220,113 @@ export default {
     $('.toc select.dropdown').dropdown({
       action: 'combo'
     })
-    var vue = this
-    var toggleView = function ($btn) {
-      $btn.addClass('active')
-      var sib = $btn.siblings('.button')
-      var showCmp = $btn.data('show')
-      var hideCmp = sib.data('show')
+    $('.ui.accordion').accordion()
 
-      sib.removeClass('active')
-      var cmp = vue.$refs[showCmp]
-      if (!$(cmp.$el).transition('is visible')) {
-        $(cmp.$el).transition({
-          behavior: 'show',
-          onComplete: function () {
-            vue.$broadcast('showCmp', cmp)
-          }
-        })
+    this.setupToggleView()
+  },
+  methods: {
+    setupSliders: function () {
+      const ceilTo = (nr, to) => {
+        return Math.ceil(nr / to) * to
       }
-      $(vue.$refs[hideCmp].$el).transition('hide')
-    }
+      const update = (attr, data) => {
+        this.$set(attr, [data.from, data.to])
+      }
+      const cfg = {
+        min: 0,
+        from: 0,
+        type: 'double',
+        grid: true
+      }
+      const createSlider = (sel, max, step, attr, prefix, postfix) => {
+        $(sel).ionRangeSlider(_.extend({
+          max: max,
+          to: max,
+          step: step,
+          prefix: prefix || '',
+          postfix: postfix || '',
+          onFinish: update.bind(this, attr)
+        }, cfg))
+      }
+      const maxPr = ceilTo(this.maxValues.pr, 10000)
+      const maxPrMq = ceilTo(this.maxValues.prMq, 500)
+      const maxMq = ceilTo(this.maxValues.mq, 5)
+      createSlider('#priceSlider', maxPr, 20000, 'priceRange', '€')
+      createSlider('#pricemqSlider', maxPrMq, 500, 'priceMqRange', '€')
+      createSlider('#locSlider', this.maxValues.loc, 1, 'locRange')
+      createSlider('#mqSlider', maxMq, 10, 'mqRange', '', ' m²')
+    },
+    setupToggleView: function () {
+      const vue = this
+      const toggleView = function ($btn) {
+        $btn.addClass('active')
+        const sib = $btn.siblings('.button')
+        const showCmp = $btn.data('show')
+        const hideCmp = sib.data('show')
 
-    toggleView($('.toggleView .ui.button.active'))
-    $('.toggleView .ui.button').click(function () {
-      if ($(this).hasClass('active')) return
-      toggleView($(this))
-    })
+        sib.removeClass('active')
+        const cmp = vue.$refs[showCmp]
+        if (!$(cmp.$el).transition('is visible')) {
+          $(cmp.$el).transition({
+            behavior: 'show',
+            onComplete: function () {
+              vue.$broadcast('showCmp', cmp)
+            }
+          })
+        }
+        $(vue.$refs[hideCmp].$el).transition('hide')
+      }
+
+      toggleView($('.toggleView .ui.button.active'))
+      $('.toggleView .ui.button').click(function () {
+        if ($(this).hasClass('active')) return
+        toggleView($(this))
+      })
+    }
   }
 }
 
 </script>
 
-<style>
+<style lang="less">
 #app {
   color: #2c3e50;
   font-family: Source Sans Pro, Helvetica, sans-serif;
   display: flex;
   flex-direction: row;
-}
 
-#app > .toc {
+  > .toc {
     flex: 0 0 auto;
     position: relative;
     width: 280px;
     z-index: 1;
+  }
+
+  .menu .item.header {
+    padding: 0 0.2em;
+    text-align: center;
+  }
 }
 
-#app .menu .item.header {
-  padding: 0 0.2em;
+.toc {
+  .item h3 {
+    margin-bottom: 5px;
+  }
+  .ui.styled.accordion {
+    border-radius: 0;
+    box-shadow: initial;
+  }
+
+  .ui.styled.accordion .content, .ui.styled.accordion .accordion .content {
+    padding: 0.5em 0em 1.5em;
+  }
+
+  .irs-from, .irs-to, .irs-single {
+    background: #3B97D3;
+    &:after {
+      border-top-color: #3B97D3;
+    }
+  }
 }
 
 body .ui.vertical.menu {
